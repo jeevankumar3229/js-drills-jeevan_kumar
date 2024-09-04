184215eaa1eb29d305862058a2bd65d48fb750c9
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-04 15:46:13 +0530

    Commiting file persons.js, answers.js and test.js

---
+++ drill1/answers.js
@@ -0,0 +1,147 @@
+const { arrayOfObjects } = require("./persons.js");
+
+//Given the dataset of individuals, write a function that accesses and returns the email addresses of all individuals.
+function returnEmailaddress(){
+    console.log("Emails of all Individuals:");
+    let arr1=[];
+    for(let i=0;i<arrayOfObjects.length;i++){
+        if(arrayOfObjects[i].hasOwnProperty("email")){
+        arr1.push(arrayOfObjects[i].email);
+        }
+        else{
+            console.log("Individual at ID "+arrayOfObjects[i].id+" has no email property");
+        }
+    }
+    return arr1;
+}
+
+//    Implement a function that retrieves and prints the hobbies of individuals with a specific age, say 30 years old.
+function printHobbies(age=30){
+     for(let i=0;i<arrayOfObjects.length;i++){
+        if(arrayOfObjects[i].hasOwnProperty("age")){
+            
+         if(arrayOfObjects[i].age === age){
+            if(arrayOfObjects[i].hasOwnProperty("hobbies")){
+             console.log("Hobbies of individual of age 30: "+arrayOfObjects[i].hobbies);
+            }
+            else{
+                console.log("Individual of age 30 with ID "+arrayOfObjects[i].id+" has no hobbies property");
+            }
+         }
+        }
+     }
+}
+
+
+//    Create a function that extracts and displays the names of individuals who are students (`isStudent: true`) and live in Australia.
+function displayNames(){
+     for(let i=0;i<arrayOfObjects.length;i++){
+        if(arrayOfObjects[i].hasOwnProperty("isStudent") && arrayOfObjects[i].hasOwnProperty("country")){
+         if(arrayOfObjects[i].isStudent === true && arrayOfObjects[i].country === 'Australia'){
+            if(arrayOfObjects[i].hasOwnProperty("name")){
+             console.log("Names of the individuals who are students and live in Australia: "+arrayOfObjects[i].name);
+            }
+            else{
+                console.log("Individual who is a student and lives in Australia with ID "+arrayOfObjects[i].id+" has no name property");
+            }
+         }
+     }
+}
+}
+
+//    Write a function that accesses and logs the name and city of the individual at the index position 3 in the dataset.
+
+function logsName(index=3){
+     for(let i=0;i<arrayOfObjects.length;i++){
+         if(index === i){
+            if(arrayOfObjects[i].hasOwnProperty("name") && arrayOfObjects[i].hasOwnProperty("city")){
+             console.log("Name and City of Individual at specified index position: name: "+arrayOfObjects[i].name +" city: "+arrayOfObjects[i].city);
+         }
+         else{
+            if(arrayOfObjects[i].hasOwnProperty("name")){
+                console.log("Name of Individual at specified index position: name: "+arrayOfObjects[i].name);
+            }
+            else if(arrayOfObjects[i].hasOwnProperty("city")){
+                console.log("City of Individual at specified index position: city: "+arrayOfObjects[i].city);
+            }
+            else{
+            console.log("Individual at ID "+arrayOfObjects[i].id +" has no name and city property")
+            }
+         }
+     }
+     
+}
+}
+
+//    Implement a loop to access and print the ages of all individuals in the dataset.
+function printAges(){
+    console.log("Ages of all individuals: ");
+     for(let i=0;i<arrayOfObjects.length;i++){
+        if(arrayOfObjects[i].hasOwnProperty("age")){
+         console.log(arrayOfObjects[i].age);
+        }
+        else{
+            console.log("Individual at ID "+arrayOfObjects[i].id+" has no age property");
+        }
+     }
+    
+}
+
+//    Create a function to retrieve and display the first hobby of each individual in the dataset.
+function displayFirstHobby(){
+    console.log("First hobby of each individual: ");
+     for(let i=0;i<arrayOfObjects.length;i++){
+        if(arrayOfObjects[i].hasOwnProperty("hobbies") && arrayOfObjects[i]["hobbies"].length > 0){
+         console.log(arrayOfObjects[i]["hobbies"][0]);
+        }
+        else{
+            console.log("Individual at ID "+arrayOfObjects[i].id+" has no hobby property or hobbies list is empty");
+        }
+     }
+}
+
+//    Write a function that accesses and prints the names and email addresses of individuals aged 25.
+function printNamesAndEmails(age=25){
+     for(let i=0;i<arrayOfObjects.length;i++){
+        if(arrayOfObjects[i].hasOwnProperty("age")){
+         if(arrayOfObjects[i].age === age){
+            if(arrayOfObjects[i].hasOwnProperty("name")&& arrayOfObjects[i].hasOwnProperty("email")){
+             console.log("Name and Email of individual of specified age: name: "+arrayOfObjects[i].name +" email: "+arrayOfObjects[i].email);
+         }
+         else{
+            if(arrayOfObjects[i].hasOwnProperty("name")){
+                console.log("Name of Individual of age 25 having ID "+arrayOfObjects[i].id +" is " +arrayOfObjects[i].name);
+            }
+            else if(arrayOfObjects[i].hasOwnProperty("email")){
+                console.log("Email of Individual of age 25 having ID "+arrayOfObjects[i].id +" is " +arrayOfObjects[i].email);
+            }
+            else{
+                console.log("Individual at ID "+arrayOfObjects[i].id +" of age 25 has no name and city property")
+            }
+         }
+         }
+        }
+     }
+ }
+
+//    Implement a loop to access and log the city and country of each individual in the dataset.
+function logCityAndCountry(){
+    console.log("The city and the Country of all individuals: ");
+     for(let i=0;i<arrayOfObjects.length;i++){
+        if(arrayOfObjects[i].hasOwnProperty("city") && arrayOfObjects[i].hasOwnProperty("country")){
+         console.log("city: "+arrayOfObjects[i].city+ " country: "+arrayOfObjects[i].country);
+        }
+        else{
+            if(arrayOfObjects[i].hasOwnProperty("city")){
+                console.log("city of individual with ID "+arrayOfObjects[i].id+" is "+arrayOfObjects[i].city);
+            }
+            else if(arrayOfObjects[i].hasOwnProperty("country")){
+                console.log("Country of individual with ID "+arrayOfObjects[i].id+" is "+arrayOfObjects[i].country);
+            }
+            else{
+            console.log("Individual at ID "+arrayOfObjects[i].id +" has no city and country property")
+            }
+        }
+     }
+}
+module.exports = {returnEmailaddress, printHobbies, displayNames, logsName, printAges,displayFirstHobby, printNamesAndEmails, logCityAndCountry}
---
+++ drill1/test.js
@@ -0,0 +1,10 @@
+const {arrayOfObjects} =require("./persons.js");
+const {returnEmailaddress, printHobbies, displayNames, logsName, printAges,displayFirstHobby, printNamesAndEmails, logCityAndCountry} = require('./answers.js')
+console.log(returnEmailaddress());
+printHobbies();
+displayNames();
+logsName();
+printAges();
+displayFirstHobby();
+printNamesAndEmails();
+logCityAndCountry();
