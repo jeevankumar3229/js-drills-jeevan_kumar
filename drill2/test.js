c3067fbd509efcded5872450004fe7ee169d2245
Jeevan Kumar <jeevankumar3229@gmail.com>
2024-09-05 11:21:50 +0530

    modify test.js

--- drill2/test.js
+++ drill2/test.js
@@ -1,5 +1,5 @@
 const { findAllUsers, findAllUsersInGermany, findAllUsersWithMasterDegree, findUsersOnProgrammingLanguage } = require('./answers.js');
-//console.log(findAllUsers());
-//console.log(findAllUsersInGermany());
-//console.log(findAllUsersWithMasterDegree());
+console.log(findAllUsers());
+console.log(findAllUsersInGermany());
+console.log(findAllUsersWithMasterDegree());
 console.log(findUsersOnProgrammingLanguage());
